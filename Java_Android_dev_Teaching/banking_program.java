bc8590b825075ebe5bfbe10b39d2476e0310b3d9
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2026-02-13 22:30:52 +0500

    Update banking_program.java

--- Java_Android_dev_Teaching/banking_program.java
+++ Java_Android_dev_Teaching/banking_program.java
@@ -1,15 +1,5 @@
 /*
-Welcome to JDoodle!
-
-You can execute code here in 88 languages. Right now you’re in the Java IDE.
-
-  1. Click the orange Execute button ▶ to execute the sample code below and see how it works.
-
-  2. Want help writing or debugging code? Type a query into JDroid on the right hand side ---------------->
-
-  3.Try the menu buttons on the left. Save your file, share code with friends and open saved projects.
-
-Want to change languages? Try the search bar up the top.
+banking app program
 */
 import java.util.*;
 
@@ -25,7 +15,7 @@ public class BankApp {
         System.out.println("\n-4.Exit");
         System.out.println("\n choose option");
         
-    }
+    
         int choice = sc.nextInt(); // get the input as integers
         sc.nextLine();
         switch(choice){
@@ -36,15 +26,15 @@ public class BankApp {
                 System.out.println("Goodbye");
                 return;
             }
-            default->
             
-            System.out.println("Your choice is invalid");
+           
+            default -> System.out.println("Your choice is invalid");
+                }
+            }
         }
-    }
-  }
-  static void addCustomer(){
+     static void addCustomer(){
     System.out.println(" Enter your name ");
-    String name=sc.nextline();
+    String name=sc.nextLine();
     System.out.println(" enter your intial/opening  balance: ");
     double balance =sc.nextDouble();
     accounts.put(name,balance);
@@ -87,6 +77,7 @@ public class BankApp {
 }
 
 }
+  
 
         
     
